fafd41b555d8630c7e05501988b005db30cd1046
yubinCloud <52986795+yubinCloud@users.noreply.github.com>
2020-02-11 21:50:51 +0800

    Update common.py

--- app/common.py
+++ app/common.py
@@ -1,2 +1,9 @@
 # 公共函数文件
 
+# 检查一个字典中是否有一系列的key
+def check_key(aDict, *keys):
+    for k in keys:
+        if k in aDict:  # 存在该键
+            continue
+        else:  # 不存在该键，抛出KeyError异常
+            raise KeyError
