8b93bdb257cbe21677e9358a1368001b0617c165
yubinCloud <52986795+yubinCloud@users.noreply.github.com>
2020-02-12 20:02:32 +0800

    Update register.py

--- app/register.py
+++ app/register.py
@@ -1,6 +1,7 @@
 import stu_apply_info
 import room_use_info
 import room_info
+import stu_room_use
 
 # 注册所有蓝本视图函数
 def register_all(app):
@@ -16,3 +17,7 @@ def register_all(app):
     # http://xx.com/api/stu/room/id
     # GET: 查看教室介绍
     app.register_blueprint(room_info.room_info, url_prefix='/api/stu/room/<string:room_id>')
+	
+	# http://xx.com/stu/room/use
+    # GET: 教室使用情况
+    app.register_blueprint(stu_room_use.stu_room_use, url_prefix='/stu/room/use')
