13d672b2321228710659d908a2995a07a4ee04d2
XQL <1203688355@qq.com>
2020-02-15 21:58:52 +0800

    增加“获取全部楼号”

--- app/register.py
+++ app/register.py
@@ -2,6 +2,8 @@ import stu_apply_info
 import room_use_info
 import room_info
 import stu_room_use
+import room_number_get
+
 
 # 注册所有蓝本视图函数
 def register_all(app):
@@ -17,7 +19,11 @@ def register_all(app):
     # http://xx.com/api/stu/room/id
     # GET: 查看教室介绍
     app.register_blueprint(room_info.room_info, url_prefix='/api/stu/room/<string:room_id>')
-	
+
     # http://xx.com/stu/room/use
     # GET: 教室使用情况
     app.register_blueprint(stu_room_use.stu_room_use, url_prefix='/api/stu/room/use')
+
+    # http://xx.com/api/stu/building
+    # GET: 获取全部楼号
+    app.register_blueprint(room_number_get.room_number_get, url_prefix='/api/stu/building')
