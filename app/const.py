5349847ee0ac65d415fe8544dab66ccb09bc4bc9
XQL <1203688355@qq.com>
2020-02-15 22:35:23 +0800

    增加“获取全部楼号”

--- app/const.py
+++ app/const.py
@@ -4,8 +4,8 @@
 # 连接数据库的相关常量
 class DatabaseConst:
     MYSQL_USERNAME = 'root'
-    MYSQL_PASSWORD = 'xxx'
-    MYSQL_HOST = 'xxx'
+    MYSQL_PASSWORD = '15215512913a'
+    MYSQL_HOST = '127.0.0.1'
     MYSQL_PORT = '3306'
-    MYSQL_DBNAME = 'hitapply'
+    MYSQL_DBNAME = 'data'
     MYSQL_CHARSET = 'utf8'
