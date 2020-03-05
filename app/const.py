f42c2e5a99babccaf7ce364133cfae42eacd44b4
yubinCloud <52986795+yubinCloud@users.noreply.github.com>
2020-03-05 19:45:56 +0800

    update stu_room_use.py

--- app/const.py
+++ app/const.py
@@ -4,8 +4,8 @@
 # 连接数据库的相关常量
 class DatabaseConst:
     MYSQL_USERNAME = 'root'
-    MYSQL_PASSWORD = 'xxx'
-    MYSQL_HOST = 'xxx'
+    MYSQL_PASSWORD = 'xxxx'
+    MYSQL_HOST = 'localhost'
     MYSQL_PORT = '3306'
     MYSQL_DBNAME = 'hitapply'
     MYSQL_CHARSET = 'utf8'
--- app/stu_room_use.py
+++ app/stu_room_use.py
@@ -10,6 +10,19 @@ import common
 stu_room_use = Blueprint('stu_room_use', __name__)
 
 
+# 创建一个原始的res_data
+def create_res_data(rooms, timetable):
+    res_data = dict()
+    for room in rooms:
+        floor = str(room.floor)
+        if floor not in res_data:
+            res_data[floor] = dict()
+
+        res_data[floor][room.room_name] = dict(id=room.room_id, use=[True] * len(timetable))
+
+    return res_data
+
+
 # 将一个时间点对应到第几大节
 def time_to_class(time, timetable, is_begin=True):
     if is_begin:
@@ -34,11 +47,14 @@ def modify_room_status(room_status, record, timetable):
 # 根据申请记录修改 res_data
 def modify_res_data(res_data, records, timetable):
     for record in records:
-        room_status = res_data.get(record.room_name)
+        cur_floor = res_data.get(str(record.floor))
+        if cur_floor is None:
+            continue
+        room_status = cur_floor.get(record.room_name)
         if room_status is None:  # 说明所申请到的教室不在教室信息表中
             continue
         else:
-            modify_room_status(room_status, record, timetable)
+            modify_room_status(room_status['use'], record, timetable)
 
 
 # GET:查看教室使用情况
@@ -51,7 +67,7 @@ def stu_room_use_info():
 
     # 检查json数据是否有缺少
     try:
-        common.check_key(json_data, 'date', 'building', 'floor')
+        common.check_key(json_data, 'date', 'building')
     except KeyError:
         return jsonify(code=-101, data=None)
 
@@ -60,13 +76,13 @@ def stu_room_use_info():
 
     # 获取符合条件的全部教室 rooms 并生成初始化的结果数据 res_data
     building = json_data.get('building')
-    floor = json_data.get('floor')
-    rooms = Room.query.filter(and_(Room.building == building, Room.floor == floor))
-    res_data = {room.room_name: [True] * len(timetable) for room in rooms}
+    rooms = Room.query.filter(and_(Room.building == building))
+    # res_data = {room.room_name: [True] * len(timetable) for room in rooms}
+    res_data = create_res_data(rooms, timetable)
 
     # 查询申请信息表中符合查询条件的申请记录
     records = Apply.query.filter(and_(Apply.check_status == "审核通过", Apply.use_date == json_data.get('date'),
-                                      Apply.building == building, Apply.floor == floor)).all()
+                                      Apply.building == building)).all()
 
     # 根据申请记录修改 res_data
     modify_res_data(res_data, records, timetable)
