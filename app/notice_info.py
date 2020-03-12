c68d9e3aa30b410780f803ef136b62ad7cf3afc1
lvyyevd <46083201+lvyyevd@users.noreply.github.com>
2020-03-12 19:43:29 +0800

    Update notice_info.py

--- app/notice_info.py
+++ app/notice_info.py
@@ -8,15 +8,28 @@ notice_info = Blueprint('notice_info', __name__)
 @notice_info.route('', methods = ['GET'])
 
 def notice_info(notice_id):
-    record = Notice.query.get(notice_id)
-
-    if record is None:
-        return jsonify(code = -102, data = {})
+    if notice_id == -1:
+        length = len(Notice.query.all())
+        record = Notice.query.get(length)
+        if record is None:
+            return jsonify(code=-102, data={})
+        else:
+            data = {
+                'id': record.notice_id,
+                'title': record.org,
+                'time': turn_to_string(record.time),
+                "content": record.content
+            }
+            return jsonify(code=0, data=data)
     else:
-        data = {
-            'id': record.notice_id,
-            'title': record.org,
-            'time': turn_to_string(record.time),
-            "content": record.content
-        }
-        return jsonify(code=0, data=data)
+        record = Notice.query.get(notice_id)
+        if record is None:
+            return jsonify(code = -102, data = {})
+        else:
+            data = {
+                'id': record.notice_id,
+                'title': record.org,
+                'time': turn_to_string(record.time),
+                "content": record.content
+            }
+            return jsonify(code=0, data=data)
