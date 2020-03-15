8c32c92ef32dc492cb0a48319b4eb8ab832acd42
XQL <1203688355@qq.com>
2020-03-15 14:51:26 +0800

    修改学生端"获得全部楼号"部分

--- app/room_floor_get.py
+++ app/room_floor_get.py
@@ -9,19 +9,21 @@ room_floor_get = Blueprint('room_number_get', __name__)
 @room_floor_get.route('/', methods=['GET'])
 def room_number_fun():
     room_list = Room.query.all()  # 获取Room表所有教室信息
-    s = set()
+    s = set()   # s是楼的集合
+    dic = {}    # 用dic辅助对列表进行排序，因为两层字典内的列表无法使用sort()排序
     for i in room_list:
         s.add(i.building)
-    assist_dict = {}
-    room_data = []
+    room_data = {}
     for i in s:
-        assist_dict[i] = []
+        room_data[i] = {}
     for i in room_list:
-        if i.floor not in assist_dict[i.building]:
-            assist_dict[i.building].append(i.floor)
-    for i in assist_dict:
-        assist_dict[i].sort()
-    sort = sorted(assist_dict)
-    for i in sort:
-        room_data.append({i: assist_dict[i]})
+        if i.building+'*&'+str(i.floor) not in dic:
+            dic[i.building+'*&'+str(i.floor)] = []
+        dic[i.building+'*&'+str(i.floor)].append(i.room_name)
+    for i in dic:
+        dic[i].sort()
+        pre_suf = str(i).split('*&')
+        pre = pre_suf[0]
+        suf = pre_suf[1]
+        room_data[pre][suf] = dic[i]
     return jsonify(code=0, data=room_data)
